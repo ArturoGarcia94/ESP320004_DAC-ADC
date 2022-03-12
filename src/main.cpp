7c52e243aa838980a395486d04239b4621f7f3e7
Arturo <arturo.antonio.garcia94@gmail.com>
2022-03-12 16:30:18 -0600

    Señal seno con variacion de amplitu por ADC

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,29 @@
 #include <Arduino.h>
 
+
+#define f 500
+#define B 128
+#define PinADC 34
+#define Salida 26
+
+
+int rADC = 0;
+int Volts = 0;
+int y = 0;
+double j = 0;
+
 void setup() {
-  // put your setup code here, to run once: s{ñ} segunda confimracion
-  
+analogReadResolution(8);
+analogSetPinAttenuation(PinADC, ADC_0db);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+     
+ for(j=0; j<360; j+=0.00120){
+  rADC = analogRead(PinADC);
+  y = map(rADC, 0, 255, 50, 150);
+  dacWrite(Salida, y*sin(2*PI*f*(j*PI/180)) + B);
+  }
+
 }
